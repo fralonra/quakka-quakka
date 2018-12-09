3470796a038acd9c304e038d71e7b81717aaaa92
fralonra <fralonra@aliyun.com>
2018-12-09 17:32:21 +0800

    Updates

---
+++ precache-manifest.d160771308a0ba5f2ba5d9d8d18ffb6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "487b463b54f95503a948",
+    "url": "/quakka-quakka/static/css/main.1edfb4b6.chunk.css"
+  },
+  {
+    "revision": "487b463b54f95503a948",
+    "url": "/quakka-quakka/static/js/main.487b463b.chunk.js"
+  },
+  {
+    "revision": "2c967033f2a1c16caf3c",
+    "url": "/quakka-quakka/static/css/1.9e544c4f.chunk.css"
+  },
+  {
+    "revision": "2c967033f2a1c16caf3c",
+    "url": "/quakka-quakka/static/js/1.2c967033.chunk.js"
+  },
+  {
+    "revision": "a96f664aa5e0abddcd78",
+    "url": "/quakka-quakka/static/js/runtime~main.a96f664a.js"
+  },
+  {
+    "revision": "c6c653e562c2b35e596165cebaba87fc",
+    "url": "/quakka-quakka/index.html"
+  }
+];
